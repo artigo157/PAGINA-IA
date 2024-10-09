63b05150cb2c7b1331063e527b23e2688253e680
artigo157 <107260244+artigo157@users.noreply.github.com>
2024-10-09 11:16:58 -0300

    09/10

--- script.js
+++ script.js
@@ -6,37 +6,37 @@ const textoResultados = document.querySelector(".texto-resultados");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
+        enunciado: "Qual foi o campeão da copa do mundo de 2010?",
         alternativas: [
-        "isso é assustador!",
-        "isso é maravilhoso!"
+        "Espanha!",
+        "Alemanha!"
         ]
 
     },
     {
-        enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
+        enunciado: "Qual a seleção que mais tem copas do mundo?",
         alternativas: [
-        "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-        "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+        "Argentina.",
+        "Brasil."
         ]
 
     },
     {
-        enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz:",
+        enunciado: "qual foi a ultima seleção campeam do mundo.",
         alternativas: [
-        "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
-        "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+        "França.",
+        "Argentina."
         ]
 
 
     },
     {
-        enunciado: "Ao final da discussão, Gabriel precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "quem foi a primeira campeam do mundo?",
         alternativas: [
-        "Criar uma imagem utilizando um gerador de imagem de IA.",
-        "Criar uma imagem utilizando uma plataforma de design como o Paint."
+        "Italia.",
+        "Uruguai."
         ]
 
-    },
+    }
 
 ];
