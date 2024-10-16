e869d963c579c37388302c5e4814c027c29ab10f
artigo157 <107260244+artigo157@users.noreply.github.com>
2024-10-16 11:11:20 -0300

    16/10

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAltermativas = document.querySelector(".caixa-alternativas");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultados");
 const textoResultados = document.querySelector(".texto-resultados");
 
@@ -8,24 +8,52 @@ const perguntas = [
     {
         enunciado: "Qual foi o campeão da copa do mundo de 2010?",
         alternativas: [
-        "Espanha!",
-        "Alemanha!"
+            {
+                texto:"Espanha!",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"Alemanha!",
+                afirmacao:"afirmação."
+                
+
+            }
+        
         ]
 
     },
     {
         enunciado: "Qual a seleção que mais tem copas do mundo?",
         alternativas: [
-        "Argentina.",
-        "Brasil."
+            {
+                texto:"Brasil.",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"Argentina.",
+                afirmacao:"afirmação."
+                
+
+            }
+        
         ]
 
     },
     {
         enunciado: "qual foi a ultima seleção campeam do mundo.",
         alternativas: [
-        "França.",
-        "Argentina."
+            {
+                texto:"França.",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"Argentina.",
+                afirmacao:"afirmação."
+                
+
+            }
+        
+    
         ]
 
 
@@ -33,10 +61,43 @@ const perguntas = [
     {
         enunciado: "quem foi a primeira campeam do mundo?",
         alternativas: [
-        "Italia.",
-        "Uruguai."
+            {
+                texto:"Italia.",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"uruguai.",
+                afirmacao:"afirmação"
+                
+
+            }
+        
+        
         ]
 
     }
 
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",function(){
+            atual++;
+            mostraPergunta();
+        });
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
